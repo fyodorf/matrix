e660e45ed4a8f5b08c2bda894b520b4c819fbc42
Fyodor <fyodor.filippov@gmail.com>
2014-04-01 17:51:29 -0600

    initial commit

---
+++ matricies.py
@@ -0,0 +1,81 @@
+A = [[1.0,2.0,3.0],
+     [4.0,5.0,6.0],
+     [7.0,8.0,9.0]]
+I = [[1.0,0.0,0.0],
+     [0.0,1.0,0.0],
+     [0.0,0.0,1.0]]
+def matrixPrint(m,name):
+    print("matrix %s" %name)
+    for line in m:
+        print(line)
+matrixPrint(A,"A")
+matrixPrint(I,"I")
+def matrixAdd(a,b):
+    r=[]
+    for i in range(len(a)):
+        line = []
+        for j in range(len(a[i])):
+            line.append(a[i][j]+b[i][j])
+        r.append(line)
+    return r
+    
+matrixPrint(matrixAdd(A,I),"A+B")
+def matrixScale(a,f):
+    r=[]
+    for i in range(len(a)):
+        line = []
+        for j in range(len(a[i])):
+            line.append(a[i][j]*f)
+        r.append(line)
+    return r
+    
+matrixPrint(matrixScale(A,10),"Scale")
+def matrixTranspose(M):
+    R = []
+    for i in range(len(M[0])):
+        line = []
+        for j in range(len(M)):
+            line.append(M[j][i])
+            
+        R.append(line)
+    return R
+matrixPrint(matrixTranspose(A),"transpose")
+def matrixSubtract(x,y):
+    A = matrixScale(x,-1)
+    B = matrixAdd(A,y)
+    return B
+matrixPrint(matrixSubtract(I,A),"Subtract")
+def matrixRotate(M):
+    R = []
+    for i in range(len(M[0])):
+        line = []
+        for j in range(len(M)):
+            line.append(M[j][len(M[0])-i-1])
+            
+            
+        R.append(line)
+    return R
+matrixPrint(matrixRotate(A),"rotate")    
+
+def matrixflip(M):
+    R = []
+    for i in range(len(M[0])):
+        line = []
+        for j in range(len(M)):
+            line.append(M[i][len(M)-j-1])
+            
+            
+        R.append(line)
+    return R
+matrixPrint(matrixflip(A),"flip") 
+def matrixRotate180(M):
+    R = []
+    for i in range(len(M[0])):
+        line = []
+        for j in range(len(M)):
+            line.append(M[len(M)-i-1][len(M)-j-1])
+            
+            
+        R.append(line)
+    return R
+matrixPrint(matrixRotate180(A),"rotate180")
